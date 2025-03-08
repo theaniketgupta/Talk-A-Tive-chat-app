938b9fe70b94b3971d8549c8e3c5c7b07f10a58a
aniveny <aniketald2018@gmail.com>
2025-03-08 21:04:31 +0530

    Added deployed url in backend

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,17 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.route");
 const chatRoutes = require("./routes/chat.route");
-const messageRoutes=require('./routes/message.route')
+const messageRoutes = require("./routes/message.route");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
-const path= require('path');
+const path = require("path");
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //To accept JSON data.
 
-
+const cors = require("cors");
+app.use(cors({ origin: "https://talk-a-tive-chatapp-eta.vercel.app" }));
 
 // //------------Deployment----------------
 
@@ -31,7 +32,6 @@ app.use(express.json()); //To accept JSON data.
 //   });
 // }
 
-
 // //------------Deployment----------------
 
 app.use("/api/user", userRoutes);
@@ -43,51 +43,45 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 connectDB();
-const server= app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
-const io= require('socket.io')(server, {
-  pingTimeout: 60000, 
+const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://talk-a-tive-chatapp-eta.vercel.app",
   },
-})
+});
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
   console.log("Connected to socket.io");
 
-  socket.on('setup', (userData)=>{
-    socket.join(userData._id)
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
     console.log(userData._id);
     socket.emit("connected");
-  })
+  });
 
-  socket.on('join chat', (room)=>{
-   socket.join(room);
-   console.log("User joined room: "+ room)
-  })
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room: " + room);
+  });
 
-  socket.on('typing', (room)=>
-     socket.in(room).emit("typing")
-     )
-  socket.on('stop typing', (room)=>
-     socket.in(room).emit("stop typing")
-     )
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on('new message', (newMessageRecieved)=>{
-    var chat= newMessageRecieved.chat; 
-    
-    if(!chat.users)
-     return console.log("chat.users is not defined.") 
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
 
-     chat.users.forEach(user=>{
-      if(user._id == newMessageRecieved.sender._id) return ;
+    if (!chat.users) return console.log("chat.users is not defined.");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
 
       socket.in(user._id).emit("message recieved", newMessageRecieved);
-     })
-   }) 
-  
-   socket.off("setup", ()=>{
+    });
+  });
+
+  socket.off("setup", () => {
     console.log("USER DISCONNECTED");
     socket.leave(userData._id);
-   })
-
+  });
 });
