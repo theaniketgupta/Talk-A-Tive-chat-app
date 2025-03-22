6f2023a4a6b06badd36c77adcb923c03f6cf7569
aniveny <aniketald2018@gmail.com>
2025-03-22 20:46:04 +0530

    Added fix for login route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,10 +4,10 @@ const {
   loginUser,
   getAllUsers,
 } = require("../controllers/user.controller");
-const {auth}=require("../middlewares/authMiddleware")
+const { auth } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.route('/').post(registerUser).get(auth,getAllUsers);
-router.post('/login',loginUser);
+router.route("/").post(registerUser).get(auth, getAllUsers);
+router.route("/login").post(loginUser);
 
 module.exports = router;
