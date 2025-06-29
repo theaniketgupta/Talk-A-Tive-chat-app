80606eea021f4c83a19c38dc91cb25a32c2cc8ac
aniveny <aniketald2018@gmail.com>
2025-06-29 12:49:10 +0530

    Adde vercel.json file

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -58,7 +58,7 @@ const Login = () => {
     } catch (error) {
       toast({
         title: "Error Occured!",
-        description: error.response.data.message,
+        description: error?.response?.data?.message,
         status: "error",
         duration: 5000,
         isClosable: true,
